76ed8e0cbbb0bdeed614158771ca9c400f190191
Mauricio Goulart <mauriciogoulart23@live.com>
2021-02-15 17:26:18 -0300

    Unidade 2: Entrada e saida de dados com as classes Scanner e Formatter

---
+++ src/disciplina2/unidade2/ScannerApp.java
@@ -0,0 +1,38 @@
+package disciplina2.unidade2;
+
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class ScannerApp {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		Formatter saida = new Formatter(System.out);
+
+		System.out.println("Informa a nota 1");
+
+		float n1 = entrada.nextFloat();
+		
+		System.out.println("Informa a nota 2");
+
+		float n2 = entrada.nextFloat();
+
+		System.out.println("Informa a nota 3");
+
+		float n3 = entrada.nextFloat();
+
+		System.out.println("Informa a nota 4");
+
+		float n4 = entrada.nextFloat();
+		
+		float media = (n1+n2+n3+n4) / 4;
+		
+		saida.format("media = %.2f ", media);
+		
+		entrada.close();
+		saida.close();
+
+	}
+
+}
---
+++ src/disciplina2/unidade2/ScannerAppv2.java
@@ -0,0 +1,38 @@
+package disciplina2.unidade2;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class ScannerAppv2 {
+
+	public static void main(String[] args) {
+
+		try {
+
+			Scanner entrada = new Scanner(new FileReader(
+					"C:\\Users\\Desenvolvimento\\eclipse-workspace\\CursoJava8Web\\src\\disciplina2\\unidade2\\numeros.txt"));
+			double soma = 0;
+			String valor;
+			
+			while(entrada.hasNext()) {
+				
+				valor = entrada.next();
+				soma = soma + Double.parseDouble(valor);
+				
+			}
+			
+			entrada.close();
+			JOptionPane.showMessageDialog(null, "A Soma dos valores e:" + soma);
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+		
+		
+
+	}
+
+}
